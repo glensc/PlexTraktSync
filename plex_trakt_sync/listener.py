fdbb4dcf091baa273ade37e69fe8cd357d13cf71
Elan Ruusamäe <glen@pld-linux.org>
2021-06-23 20:52:07 +0300

    Add EventFactory class

--- plex_trakt_sync/listener.py
+++ plex_trakt_sync/listener.py
@@ -18,6 +18,26 @@ EVENTS = {
 }
 
 
+class EventFactory:
+    def get_events(self, message):
+        if message["size"] != 1:
+            raise ValueError("Unexpected size: %r" % message)
+
+        message_type = message['type']
+        if message_type not in EVENTS:
+            return
+        class_name = EVENTS[message_type]
+        if class_name not in message:
+            return
+        for data in message[class_name]:
+            event = self.create(cls=class_name, **data)
+            yield event
+
+    def create(self, cls, **kwargs):
+        # https://stackoverflow.com/a/2827726/2314626
+        return type(cls, (object,), kwargs)
+
+
 class WebSocketListener:
     def __init__(self, plex: PlexServer, interval=1):
         self.plex = plex
